6018afb1508bb3bde576e186bbd4c4a4282bbfc4
Richard Batty <rjlbatty@gmail.com>
2015-04-30 22:08:41 +0100

    [+RC] Solution

--- main.hs
+++ main.hs
@@ -2,23 +2,25 @@ import Data.Maybe
 import Data.List
 
 type Place = Char
-type LabelledPlace = (Place, Integer)
-type Neighbours = (Place, [LabelledPlace])
 type Network = [Neighbours]
-
-places = ['b', 'c']
-start = 'a'
-
-
-
---travel _ [] _ = 0
---travel start placesToVisit network = map  placesToVisit
 
 myNetwork :: Network
 myNetwork = [('a', [('b', 10), ('c', 11)]),
-             ('b', [('a', 10)]),
-             ('c', [('a', 11)])]
+             ('b', [('a', 10), ('c', 22)]),
+             ('c', [('a', 11), ('b', 22)])]
 
 getDistance :: Place -> Place -> Network -> Integer
 getDistance a b network = getLabelledPlaces b $ getLabelledPlaces a network
                           where getLabelledPlaces place network = fromJust $ lookup place network
+
+shortestRouteLength :: [Place] -> Place -> Network -> Integer
+shortestRouteLength [] _ _ = 0
+shortestRouteLength (a:[]) start network = getDistance a start network
+shortestRouteLength places start network = minimum $ map (\place -> getDistance start place network + shortestRouteLength (filter (place /=) places) place network) places
+
+placesInNetwork :: Network -> [Place]
+placesInNetwork = map fst
+
+shortestRoute :: Network -> Place -> Integer
+shortestRoute network start = shortestRouteLength (filter (start /=) (placesInNetwork network)) start network
+
